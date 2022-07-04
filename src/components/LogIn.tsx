14bfb7d90507cd06fbac036af062d54cd21cdd95
Benjamin Bascary <benjaminbascary@gmail.com>
2022-07-04 16:59:02 -0300

    Adds styling, loading spinner and responsive design to the login logout components

--- src/components/LogIn.tsx
+++ src/components/LogIn.tsx
@@ -12,7 +12,9 @@ import {
     FormHelperText, 
     FormLabel, 
     Heading, 
+    HStack, 
     Input, 
+    Spinner, 
     Text 
 } from '@chakra-ui/react';
 
@@ -20,12 +22,15 @@ import {
 export const Login = () => {
 
     const [userState, setUserState] = useState<LogInType>({email: '', password: ''});
+    const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<any>();
     const { logIn } = useUserAuth();
     const navigate = useNavigate();
 
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
+        setLoading(true)
+        setTimeout(() => setLoading(false), 500)
         setError('');
         try {
             await logIn(userState.email, userState.password);
@@ -41,52 +46,62 @@ export const Login = () => {
 
 
     return (
-        <>
-            <Heading display='flex' justifyContent='center' m='2vh'>Log In</Heading>
-            <Box display='flex' justifyContent='center' alignItems='center'>
-                <Box>
-                {error && <Alert 
-                            status='error' 
-                            mb='2vh' 
-                            borderRadius='1vh'
-                        >
-                        <AlertIcon />
-                        {error.message}
-                        </Alert>}
-                    <FormControl m='0vh 0vh 5vh 0vh'>
-                        <FormLabel htmlFor='email'>Email address</FormLabel>
-                        <Input id='email' type='email' name='email' value={userState.email} onChange={handleChange}/>
-                        <FormHelperText>We'll never share your email.</FormHelperText>
-                    </FormControl>
-                    <FormControl m='2vh 0vh 2vh 0vh'>
-                        <FormLabel htmlFor='email'>Password</FormLabel>
-                        <Input id='password' type='password' name='password' value={userState.password} onChange={handleChange} />
-                        <FormHelperText>Enter your password.</FormHelperText>
-                    </FormControl>
-                    <Box display='flex' justifyContent='center' mb='2vh'>
-                        <Button colorScheme='blue' onClick={handleSubmit}>
-                            Log In
-                        </Button>
-                    </Box>
-                    <Box display='flex' flexDirection='column'>
-                        <Box>
-                        <GoogleButton
-                            className="g-btn"
-                            type="dark"
-                            />
+        <HStack className='login-logout-container'>
+            <Box className='login-logout-left-container' width='70vh' height='100vh' backgroundColor='blue.200'>
+                
+            </Box>
+            <Box className='spinner-container' width='20vh' height='100vh' display='flex' flexDirection='column' justifyContent='center' alignItems='end'>
+                {
+                    loading && <Spinner className='spinner-container'/>
+                }
+            </Box>
+            <Box width='110vh'>
+                <Heading display='flex' justifyContent='center' m='2vh'>Log In</ Heading>
+                <Box display='flex' justifyContent='center' alignItems='center'>
+                    <Box>
+                    {error && <Alert 
+                                status='error' 
+                                mb='2vh' 
+                                borderRadius='1vh'
+                            >
+                            <AlertIcon />
+                            {error.message}
+                            </Alert>}
+                        <FormControl m='0vh 0vh 5vh 0vh'>
+                            <FormLabel htmlFor='email'>Email address</FormLabel>
+                            <Input id='email' type='email' name='email' value={userState.email} onChange={handleChange}/>
+                            <FormHelperText>We'll never share your email.</FormHelperText>
+                        </FormControl>
+                        <FormControl m='2vh 0vh 2vh 0vh'>
+                            <FormLabel htmlFor='email'>Password</FormLabel>
+                            <Input id='password' type='password' name='password' value={userState.password} onChange={handleChange} />
+                            <FormHelperText>Enter your password.</FormHelperText>
+                        </FormControl>
+                        <Box display='flex' justifyContent='center' mb='2vh'>
+                            <Button colorScheme='blue' onClick={handleSubmit}>
+                                Log In
+                            </Button>
                         </Box>
-                        <Box display='flex' flexDirection='row'>
-                            <Text>
-                                Don't have an account?   
-                            </Text>
-                            <Text cursor='pointer' color='blue' m='0vh 0vh 0vh 1vh' onClick={() => navigate('/signup')}>
-                                Sign up
-                            </Text>
+                        <Box display='flex' flexDirection='column'>
+                            <Box>
+                            <GoogleButton
+                                className="g-btn"
+                                type="dark"
+                                />
+                            </Box>
+                            <Box display='flex' flexDirection='row'>
+                                <Text>
+                                    Don't have an account?   
+                                </Text>
+                                <Text cursor='pointer' color='blue' m='0vh 0vh 0vh 1vh' onClick={() => navigate('/signup')}>
+                                    Sign up
+                                </Text>
+                            </Box>
+                            
                         </Box>
-                        
                     </Box>
                 </Box>
             </Box>
-        </>
+        </HStack>
     )
 }
--- src/components/SignUp.tsx
+++ src/components/SignUp.tsx
@@ -11,7 +11,9 @@ import {
     Heading,
     Text,
     Alert,
-    AlertIcon
+    AlertIcon,
+    HStack,
+    Spinner
 } from '@chakra-ui/react';
 
 type UserState = {
@@ -22,6 +24,7 @@ type UserState = {
 export const SignUp = (): JSX.Element => {
 
     const [userState, setUserState] = useState<UserState>({email: '', password: ''})
+    const [loading, setLoading] = useState<boolean>(false);
     const [error, setError] = useState<any>();
     const { signUp } = useUserAuth();
     const navigate = useNavigate();
@@ -32,6 +35,8 @@ export const SignUp = (): JSX.Element => {
     
     const handleSubmit = async (event: React.FormEvent) => {
         event.preventDefault();
+        setLoading(true)
+        setTimeout(() => setLoading(false), 500)
         setError('')
         try {
             await signUp(userState.email, userState.password);
@@ -43,43 +48,53 @@ export const SignUp = (): JSX.Element => {
     };
 
     return (
-        <>
-            <Heading display='flex' justifyContent='center' m='2vh'>Sing Up</Heading>
-            <Box display='flex' justifyContent='center' alignItems='center'>
-                <Box>
-                {error && <Alert 
-                            status='error' 
-                            mb='2vh' 
-                            borderRadius='1vh'
-                        >
-                        <AlertIcon />
-                        {error.message}
-                        </Alert>}
-                    <FormControl m='0vh 0vh 5vh 0vh'>
-                        <FormLabel htmlFor='email'>Email address</FormLabel>
-                        <Input id='email' type='email' name='email' value={userState.email} onChange={handleChange}/>
-                        <FormHelperText>We'll never share your email.</FormHelperText>
-                    </FormControl>
-                    <FormControl m='5vh 0vh 5vh 0vh'>
-                        <FormLabel htmlFor='email'>Password</FormLabel>
-                        <Input id='password' type='password' name='password' value={userState.password} onChange={handleChange} />
-                        <FormHelperText>Enter your password.</FormHelperText>
-                    </FormControl>
-                    <Box display='flex' justifyContent='center'>
-                        <Button colorScheme='teal' onClick={handleSubmit}>
-                            Sign Up
-                        </Button>
-                    </Box>
-                    <Box display='flex'>
-                        <Text>
-                            Already have an account?    
-                        </Text>
-                        <Text cursor='pointer' color='blue' m='0vh 0vh 0vh 1vh' onClick={() => navigate('/')}>
-                            Log In
-                        </Text>
+        <HStack>
+            <Box className='login-logout-left-container' width='70vh' height='100vh' backgroundColor='green.100'>
+
+            </Box>
+            <Box className='spinner-container' width='20vh' height='100vh' display='flex' flexDirection='column' justifyContent='center' alignItems='end'>
+                {
+                    loading && <Spinner className='spinner-container'/>
+                }
+            </Box>
+            <Box display='flex' flexDirection='column' height='100vh' width='110vh' justifyContent='center'>
+                <Heading display='flex' justifyContent='center' m='2vh'>Sing Up</Heading>
+                <Box display='flex' justifyContent='center' alignItems='center' flexDirection='column'>
+                    <Box>
+                    {error && <Alert 
+                                status='error' 
+                                mb='2vh' 
+                                borderRadius='1vh'
+                            >
+                            <AlertIcon />
+                            {error.message}
+                            </Alert>}
+                        <FormControl m='0vh 0vh 5vh 0vh'>
+                            <FormLabel htmlFor='email'>Email address</FormLabel>
+                            <Input id='email' type='email' name='email' value={userState.email} onChange={handleChange}/>
+                            <FormHelperText>We'll never share your email.</FormHelperText>
+                        </FormControl>
+                        <FormControl m='5vh 0vh 5vh 0vh'>
+                            <FormLabel htmlFor='email'>Password</FormLabel>
+                            <Input id='password' type='password' name='password' value={userState.password} onChange={handleChange} />
+                            <FormHelperText>Enter your password.</FormHelperText>
+                        </FormControl>
+                        <Box display='flex' justifyContent='center'>
+                            <Button colorScheme='teal' onClick={handleSubmit}>
+                                Sign Up
+                            </Button>
+                        </Box>
+                        <Box display='flex'>
+                            <Text>
+                                Already have an account?    
+                            </Text>
+                            <Text cursor='pointer' color='blue' m='0vh 0vh 0vh 1vh' onClick={() => navigate('/')}>
+                                Log In
+                            </Text>
+                        </Box>
                     </Box>
                 </Box>
             </Box>
-        </>
+        </HStack>
     )
 }
